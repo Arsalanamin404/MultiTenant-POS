c796d3e5b716b0930cd768b84c72dda6ef91bf5b
Arslanamin404 <arsalanrather.dev@gmail.com>
2026-07-18 22:47:06 +0530

    feat(exception): add InvalidOperationException

---
+++ src/main/java/com/arsalan/tenanttable/exception/InvalidOperationException.java
@@ -0,0 +1,7 @@
+package com.arsalan.tenanttable.exception;
+
+public class InvalidOperationException extends RuntimeException {
+    public InvalidOperationException(String msg) {
+        super(msg);
+    }
+}
