a513ad25a64f89fc9734deae9d009644b4f40891
Arslanamin404 <arsalanrather.dev@gmail.com>
2026-07-28 10:14:25 +0530

    refactor(report): add pattern to date field

--- src/main/java/com/arsalan/tenanttable/reports/dto/ReportFilterRequestDto.java
+++ src/main/java/com/arsalan/tenanttable/reports/dto/ReportFilterRequestDto.java
@@ -2,14 +2,17 @@ package com.arsalan.tenanttable.reports.dto;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
 public class ReportFilterRequestDto {
     @NotNull(message = "From date is required.")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate fromDate;
 
     @NotNull(message = "To date is required.")
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
     private LocalDate toDate;
 }
